d5ed18d98a2f58715fef8595a0d625c958e92d58
Jacob Adlers <jadlers@kth.se>
2016-05-07 20:33:03 +0200

    Add method to create field closes #8

--- code/Field.java
+++ code/Field.java
@@ -3,7 +3,19 @@
  * shape. It's this field that you want to make safer.
  */
 public class Field {
-	public Field() {
+	private Square[][] field;
 
+	public Field(int width, int height) {
+		field = createField(width, height);
+	}
+
+	private Square[][] createField(int width, int height) {
+		Square[][] filledField = new Square[width][height];
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				filledField[x][y] = new Square();
+			}
+		}
+		return filledField;
 	}
 }
