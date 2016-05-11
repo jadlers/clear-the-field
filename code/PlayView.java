1980a686423c837533197710283eace9dbada4bc
Jacob Adlers <jadlers@kth.se>
2016-05-11 22:11:20 +0200

    Add PlayView with updateField method closes #11
    Creates a new window in which the game will be played in. The class has
    a method updateScene to draw the field on a scene and update the view
    to show whats drawn.
    
    Each SquareFigure is programed so that if it’s clicked it calls the
    click(x, y) method with the x & y values of the clicked Square in the
    field and then calls the updateField to show the new scene.

---
+++ code/PlayView.java
@@ -0,0 +1,40 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * A window for playing the game
+ */
+public class PlayView {
+	Stage window;
+	Field field;
+	final int size = 40;
+	final int margin = 5;
+
+	public PlayView() {
+		window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		field = new Field(10, 10, 2);
+		updateScene();
+	}
+
+	public void updateScene() {
+		Scene scene = updateField();
+		window.setScene(scene);
+		window.show();
+	}
+
+	public Scene updateField() {
+		Group layout = new Group();
+		for (int x = 0; x < field.getField().length; x++) {
+			for (int y = 0; y < field.getField()[0].length; y++) {
+				SquareFigure figure = field.createSquareFigure(x, y, size, size);
+				figure.setLayoutX(x*size+x*margin);
+				figure.setLayoutY(y*size+y*margin);
+				layout.getChildren().add(figure);
+			}
+		}
+		return new Scene(layout, 600, 500);
+	}
+}
