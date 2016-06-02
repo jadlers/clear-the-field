9b000750523229a16f319c0bce84df5d8ed52cf9
Jacob Adlers <jadlers@kth.se>
2016-06-02 13:20:05 +0200

    Add reader & writer for saves

--- code/Scoreboard.java
+++ code/Scoreboard.java
@@ -1,14 +1,20 @@
+import java.io.*;
+
 /**
  * Keeps the best local scores of all time. The scores with name
  * is saved in a file called highScores. Will show the top 10? scores.
  */
 public class Scoreboard {
+	private Reader reader;
+	private Writer writer;
+	private String FILENAME = "highScore.txt";
 
 	/**
 	 * Shows the current scoreboard.
 	 */
 	public Scoreboard() {
-
+		createReader();
+		createWriter();
 	}
 
 	/**
@@ -22,5 +28,26 @@ public class Scoreboard {
 			this.name = name;
 			this.time = time;
 		}
+	}
+
+	/**
+	 * Initializes the reader for use when importing the current high scores.
+	 * @return initialised reader
+	 */
+	private void createReader() {
+		try{
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(FILENAME), "utf-8"));
+		} catch (IOException e) {/* Report */}
+	}
+
+	/**
+	 * Initializes the writer for use when updating the high scores.
+	 * @return initialised writer
+	 */
+	private void createWriter() {
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(FILENAME), "utf-8"));
+		} catch (IOException ex) {/* Report */}
 	}
 }
